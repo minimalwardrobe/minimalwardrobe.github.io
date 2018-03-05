270314050f577549c96bc3abea2583db0c120dde
Jack Burum <jtburum@gmail.com>
2018-03-04 19:38:26 -0800

    added some hover states and things

--- scripts/script.js
+++ scripts/script.js
@@ -2,18 +2,9 @@ $(document).ready(function() {
   fadeOutText()
   closeCard()
   catalogHeaderSelect()
-  checkForCatalogString()
-  typeWriter("Simplify your wardrobe with functional, affordable clothing that won't go out of style.", 0)
+  typeWriter("Simplify your wardrobe with functional clothing that hopefully won't go out of style.", 0)
 })
 
-function checkForCatalogString() {
-  // if (window.location.href == "file:///Users/jackburum/one-day-websites/the-minimalist-wardrobe/index.html#catalog") {
-  //   // console.log('hey');
-  //   $('html, body').animate({
-  //     scrollTop: $('.more-life-container').offset().top
-  //   }, 'slow');
-  // }
-}
 
 function catalogHeaderSelect() {
   $('.catalog-header').click(function() {
